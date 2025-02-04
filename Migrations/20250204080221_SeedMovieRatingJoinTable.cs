471b4891c4637932ca3ccba39293d2ddaf299042
yitzhak <matias6@gmail.com>
2025-02-04 13:38:58 -0400

    migrations

---
+++ Migrations/20250204080221_SeedMovieRatingJoinTable.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieManagementApi.Migrations
+{
+    public partial class SeedMovieRatingJoinTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActorMovie",
+                columns: table => new
+                {
+                    ActorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MovieId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActorMovie", x => new { x.ActorId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_ActorMovie_Actors_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActorMovie_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieRatings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MovieId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rating = table.Column<double>(type: "REAL", nullable: false),
+                    Score = table.Column<int>(type: "INTEGER", nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieRatings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieRatings_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 1, new DateTime(1974, 11, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Leonardo DiCaprio" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 2, new DateTime(1974, 1, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), "Christian Bale" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 3, new DateTime(1964, 9, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Keanu Reeves" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 4, new DateTime(1963, 12, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "Brad Pitt" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 5, new DateTime(1948, 12, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Samuel L. Jackson" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 6, new DateTime(1964, 4, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), "Russell Crowe" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 7, new DateTime(1975, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "Kate Winslet" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 8, new DateTime(1940, 4, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Al Pacino" });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 9, new DateTime(1965, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Robert Downey Jr." });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "DateOfBirth", "Name" },
+                values: new object[] { 10, new DateTime(1981, 6, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Chris Evans" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 1, "Sci-Fi Thriller", new DateTime(2010, 7, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Inception" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 2, "Space Adventure", new DateTime(2014, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), "Interstellar" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 3, "Action Thriller", new DateTime(2008, 7, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Dark Knight" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 4, "Cyberpunk Sci-Fi", new DateTime(1999, 3, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Matrix" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 5, "Psychological Drama", new DateTime(1999, 10, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fight Club" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 6, "Crime Drama", new DateTime(1994, 10, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), "Pulp Fiction" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 7, "Historical Drama", new DateTime(2000, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "Gladiator" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 8, "Romance/Drama", new DateTime(1997, 12, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), "Titanic" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 9, "Mafia Drama", new DateTime(1972, 3, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Godfather" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "ReleaseDate", "Title" },
+                values: new object[] { 10, "Superhero Action", new DateTime(2019, 4, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "Avengers: Endgame" });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 1, 2 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 2, 2 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 3, 3 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 4, 4 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 5, 5 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 6, 6 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 7, 7 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 8, 8 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 9, 9 });
+
+            migrationBuilder.InsertData(
+                table: "ActorMovie",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[] { 10, 10 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 1, "Amazing!", 1, 0.0, 9 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 2, "Masterpiece!", 2, 0.0, 10 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 3, "Great action!", 3, 0.0, 9 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 4, "Mind-bending!", 4, 0.0, 8 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 5, "Unique storytelling!", 5, 0.0, 8 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 6, "Iconic!", 6, 0.0, 9 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 7, "Epic!", 7, 0.0, 10 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 8, "Classic love story!", 8, 0.0, 7 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 9, "Legendary!", 9, 0.0, 10 });
+
+            migrationBuilder.InsertData(
+                table: "MovieRatings",
+                columns: new[] { "Id", "Comment", "MovieId", "Rating", "Score" },
+                values: new object[] { 10, "Fantastic ending!", 10, 0.0, 9 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActorMovie_MovieId",
+                table: "ActorMovie",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieRatings_MovieId",
+                table: "MovieRatings",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActorMovie");
+
+            migrationBuilder.DropTable(
+                name: "MovieRatings");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
